c6eb56e34d5bd5cbc3d2affdc7a4c802efe5409c
biko <ahmadyaslam925@gmail.com>
2023-03-23 07:13:58 +0300

    final pls344 commit

--- app.js
+++ app.js
@@ -1,63 +1,70 @@
-import express from 'express';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
-import mailchimp from "@mailchimp/mailchimp_marketing";
-var HTTP_PORT = process.env.PORT || 3000;
-import * as dotenv from 'dotenv'
-dotenv.config()
- 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const app = express();
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
- 
-const listId = process.env.MAILCHIMP_LISTID;
- 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/signup.html");
-});
- 
+const express = require('express');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+require('dotenv').config();
+
+const listId = process.env.MLIST_ID;
+
+
 mailchimp.setConfig({
-    apiKey: process.env.MAILCHIMP_APIKEY,
-    server: process.env.MAILCHIMP_SERVER,
+  apiKey: process.env.MAPI_KEY,
+  server: process.env.MSERVER_ID,
 });
- 
-app.post("/", function (req, res) {       
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function(req, res)  {
     const firstName = req.body.fName;
     const lastName = req.body.lName;
-    const email = req.body.inputEmail;
-     
-    const subscribingUser = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email
+    const email = req.body.email;
+
+const subscribingUser = {
+  firstName: firstName,
+  lastName: lastName,
+  email: email,
+};
+
+async function run() {
+  const response = await mailchimp.lists.addListMember(listId, {
+    email_address: subscribingUser.email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: subscribingUser.firstName,
+      LNAME: subscribingUser.lastName
     }
- 
-    const run = async () => {
-        const response = await mailchimp.lists.addListMember(listId, {
-            email_address: subscribingUser.email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: subscribingUser.firstName,
-                LNAME: subscribingUser.lastName
-            }
-        });
-        res.sendFile(__dirname + "/success.html")
-        console.log("Successfully added contact as an audience member. The contact's id is "+response.id+".");
-        app.post("/success", function (req, res) {
-            res.redirect("/");
-        });
-    };
-    
-    run().catch(e => {console.log(e);res.sendFile(__dirname + "/failure.html")});
-    app.post("/failure", function (req, res) {
-        res.redirect("/");
-    });    
-});
- 
-app.listen(HTTP_PORT, () => {
-    console.log("Server is running on port 3000 or 8080.")
-});
-5. If your code can works at local but goes wrong with deployment, then you can read cyclic logs use this 
-run().catch(e => {console.log(e);res.sendFile(__dirname + "/failure.html")});
+  });
+
+  console.log(
+    `Successfully added contact as an audience member. The contact's id is ${
+      response.id
+    }.`
+  );
+}
+
+function successCallback(result) {
+        res.sendFile(__dirname + '/success.html')
+        }
+
+function failureCallback(error) {
+        res.sendFile(__dirname + '/failure.html')
+        console.log(error)
+        }
+
+run().then(successCallback, failureCallback);
+
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+
+app.listen(process.env.PORT || 3000)
